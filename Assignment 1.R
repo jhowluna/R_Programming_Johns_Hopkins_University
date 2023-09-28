1dba8308eb3e6b81d0cd91ae868c21bf317c0f2c
Jhonatan <jhonatansilvaluna@gmail.com>
2023-09-28 15:08:05 -0300

    update

---
+++ Assignment 1.R
@@ -0,0 +1,88 @@
+# install.packages("data.table")
+library("data.table")
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  if (c(pollutant) %in% names(dt)){
+    return(dt[, lapply(.SD, mean, na.rm = TRUE), .SDcols = pollutant][[1]])
+  } 
+}
+
+# Example usage
+pollutantmean(directory = './specdata', pollutant = 'nitrate', id = 34)
+
+
+complete <- function(directory,  id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  return(dt[complete.cases(dt), .(nobs = .N), by = ID])
+  
+}
+
+#Example usage
+complete(directory = './specdata', id = 20:30)
+
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+RNGversion("3.5.1")  
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+
+corr <- function(directory, threshold = 0) {
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(file.path(directory, list.files(path = directory, pattern="*.csv")), data.table::fread)
+  dt <- rbindlist(lst)
+  
+  # Only keep completely observed cases
+  dt <- dt[complete.cases(dt),]
+  
+  # Apply threshold
+  dt <- dt[, .(nobs = .N, corr = cor(x = sulfate, y = nitrate)), by = ID][nobs > threshold]
+  return(dt[, corr])
+}
+
+# Example Usage
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+
+
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
